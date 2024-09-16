6f6d575285040709db738560585aca2b8b051a1b
tass2t <brunog90@gmail.com>
2024-09-16 15:20:49 +0200

    ajout resize

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
 import { Application } from 'pixi.js'
+import { gameConfig } from '@/utils/gameConfig'
 
-const app = new Application()
+export const app = new Application()
+
+const resize = () => {
+    const innerWidth = window.innerWidth
+    const innerHeight = window.innerHeight
+    const minWidth = gameConfig.display.width
+    const minHeight = gameConfig.display.height
+
+    const scaleX = innerWidth < minWidth ? minWidth / innerWidth : 1
+    const scaleY = innerHeight < minHeight ? minHeight / innerHeight : 1
+    const scale = scaleX > scaleY ? scaleX : scaleY
+
+    const width = innerWidth * scale
+    const height = innerHeight * scale
+
+    app.renderer.canvas.style.width = `${width}px`
+    app.renderer.canvas.style.height = `${height}px`
+
+    window.scrollTo(0, 0)
+    app.renderer.resize(width, height)
+}
 
 await app.init({
-    background: '#1099bb',
+    background: '#111111',
     resolution: Math.max(window.devicePixelRatio, 2),
 })
 
 document.body.appendChild(app.canvas)
+
+window.addEventListener('resize', resize)
+
+resize()
---
+++ src/utils/gameConfig.ts
@@ -0,0 +1,9 @@
+const MIN_WIDTH = 375
+const MIN_HEIGHT = 667
+
+export const gameConfig = {
+    display: {
+        height: MIN_HEIGHT,
+        width: MIN_WIDTH,
+    },
+}
